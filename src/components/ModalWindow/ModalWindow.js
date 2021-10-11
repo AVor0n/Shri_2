379f7c95bf67d2a11759081a61cba5c4df06204d
Andrey Voronin <AVor0n@yandex.ru>
2021-10-11 05:11:39 +0500

    feat: add modal window

---
+++ src/components/ModalWindow/ModalWindow.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Button from '../Button/Button';
+import TextInput from '../TextInput/TextInput';
+import './ModalWindow.css';
+
+const ModalWindow = (props) => {
+  return (
+    <div className="modal__wrapper" onClick={props.handleClick}>
+      <div className="modal-content__wrapper">
+        <h2 className="title settings__title">New build</h2>
+        <TextInput
+          label="Enter the commit hash which you want to build"
+          placeholder="Commit hash"
+          hasBtnClear
+        />
+        <div className="settings__buttons">
+          <Button
+            text="Run build"
+            size="m"
+            color="action"
+            href="/"
+            handleClick={props.handlerClick}
+          />
+          <Button
+            text="Cancel"
+            size="m"
+            color="control"
+            href="/"
+            handleClick={props.handlerClick}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ModalWindow;
--- src/pages/history/index.js
+++ src/pages/history/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 import Header from '../../components/Header/Header';
 import Button from '../../components/Button/Button';
 import Footer from '../../components/Footer/Footer';
 import BuildList from '../../components/BuildList/BuildList';
+import ModalWindow from '../../components/ModalWindow/ModalWindow';
 
 const HistoryPage = ({ data }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const closeModal = () => setModalVisible(false);
+
   return (
     <React.Fragment>
+      {modalVisible ? <ModalWindow handlerClick={closeModal} /> : null}
       <Header text={data.repoName} black>
-        <Button text="Run build" size="s" icon="play" color="control" />
+        <Button
+          text="Run build"
+          size="s"
+          icon="play"
+          color="control"
+          handleClick={() => {
+            setModalVisible(true);
+            console.log(42);
+          }}
+        />
         <Button size="s" icon="gear" color="control" href="/settings" />
       </Header>
 
